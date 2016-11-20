7c2e9f7647262224ad97085a7b5bdcb00b249126
yirolhao <yirolhao@gmail.com>
2016-11-20 18:37:52 +0800

    finsh static-pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,41 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get root" do
+    get root_path
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title" , "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title" , "Help | #{get_title}" #call method
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title" , "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title" , "Contact | #{@base_title}"
+  end
+
+  def setup   # method name must is setup
+    @base_title = "GetsuKami"
+  end
+
+  def get_title  
+    @base_title = "GetsuKami"
   end
 
 end
