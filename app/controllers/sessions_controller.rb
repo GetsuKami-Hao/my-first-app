04d83f91965a829edcc466025bf621b95f47a2b8
yirolhao <yirolhao@gmail.com>
2016-11-23 15:38:52 +0800

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in? # or session[:user_id]
+      
     redirect_to root_url
   end
 
   def create
   	# render html: params.inspect
-  	user = User.find_by(email: params[:session][:email].downcase)
+  	@user = User.find_by(email: params[:session][:email].downcase)
 
-  	if user && user.authenticate(params[:session][:password])
-  		log_in user
-  		redirect_to user
+  	if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+  		redirect_to @user
   	else
   		flash.now[:danger] = 'Invalid email/password combination' 
   		render 'new'
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -43,10 +43,22 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   	delete logout_path
   	assert_not is_logged_in?
   	assert_redirected_to root_url
+
+    delete logout_path #analog user in another window click log_out
   	follow_redirect!
   	assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
-  	
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')  #define in test_helper.rb
+    # assert_not_nil cookies['remember_token']
+    assert_equal  false , assigns(:user).remember_token.nil?
+  end 
+
+  test "login without remembering" do
+    log_in_as(@user,remember_me: '0')
+    assert_nil cookies['remember_token']
   end
 end
